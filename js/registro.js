c9be89c578efc0ab1ca24a456da8f2be8b250d37
braulio valdes antiqueo <brauliomariano@gmail.com>
2024-03-03 02:42:43 -0300

    modal fallo en registro o recuperacion por existencia de wallet

--- js/registro.js
+++ js/registro.js
@@ -1,25 +1,46 @@
 // login
 $(document).ready(function () {
-    $('.form-registro').submit(function (event) {
-      event.preventDefault();
+  if (localStorage.getItem('wallet')) {
+    swal("Registro fallido", "Ya existe Cliente con Wallet, debe recuperar sus credenciales!", "error");
+  }
+  $('.form-registro').submit(function (event) {
+    event.preventDefault();
+    // si quiere volver a repetir el registro
+    if (localStorage.getItem('wallet')) {
+      swal("Registro fallido", "Ya existe Cliente con Wallet, debe recuperar sus credenciales!", "error");
+    } else {
       let nombres = $('#nombres').val();
       let apellidos = $('#apellidos').val();
+
       let email = $('#email').val();
       let password = $('#password').val();
-      // pregunta si no existe una wallet
-      if (!localStorage.getItem('wallet')) {
-        usuario = {
-            nombres: nombres,
-            apellidos: apellidos,
-            email: email,
-            // simula registro de clave/password encriptado (tipo hash bcrypt) en BD 
-            password: password
-        };
-        localStorage.setItem(email, JSON.stringify(usuario));
-        swal("Registro existoso", "Se registro correctamente el usuario!", "success");
-      } else {
-        swal("Registro fallido", "Ya existe Cliente con Wallet, debe recuperar sus credenciales!", "error");
-      }
-    });
 
+      // pregunta si no existe una wallet
+      usuario = {
+        nombres: nombres,
+        apellidos: apellidos,
+        email: email,
+        // simula registro de clave/password encriptado (tipo hash bcrypt) en BD 
+        password: password
+      };
+      localStorage.setItem(email, JSON.stringify(usuario));
+      // se crea la wallet para asociarla al usaurio creado
+      const myWallet = {
+        user: email,
+        saldo: 0,
+        trans: [],
+        destinatarios: []
+      };
+      // llimpia campos de ingreso
+      $('#nombres').val('');
+      $('#apellidos').val('');
+      $('#email').val('');
+      $('#password').val('');
+
+      // almacena la billetera
+      localStorage.setItem('wallet', JSON.stringify(myWallet));
+      swal("Registro existoso", "Se registro correctamente el usuario!", "success");
+    }
+
+  });
 });
